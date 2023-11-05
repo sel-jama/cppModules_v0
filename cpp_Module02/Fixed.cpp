1f7063d023336a546e2313f537550b6f0d74b531
Salma El Jamali <sel-jama@e2r9p1.1337.ma>
2023-11-05 16:31:16 +0100

    cpp02

--- cpp_Module02/Fixed.cpp
+++ cpp_Module02/Fixed.cpp
@@ -1,8 +1,11 @@
 #include "Fixed.hpp"
 
 Fixed::Fixed(){
-    value = 0;
-    std::cout << "Default constructor called" << std::endl;
+    std::cout << "Default constructor is called" << std::endl;
+}
+
+Fixed::Fixed(const int i){
+    //It converts it i to the corresponding fixed-point value
 }
 
 Fixed::Fixed(const Fixed& oldObj){
@@ -10,12 +13,11 @@ Fixed::Fixed(const Fixed& oldObj){
     value = oldObj.value;
 }
 
-
-Fixed& Fixed::operator=(const Fixed& oldObj){
+Fixed& Fixed::operator=(const Fixed &oldObj){
     std::cout << "Copy assignement operator called" << std::endl; 
-    if (&oldObj == this)
+    if (this == &oldObj)
         return *this;
-
+    
     value = oldObj.value;
     return *this;
 }
@@ -33,3 +35,16 @@ void Fixed::setRawBits(int const raw)
 {
     value = raw;
 }
+
+float Fixed::toFloat( void ) const{
+    //converts the fixed-point value to a floating-point value
+}
+
+int Fixed::toInt( void ) const{
+    //converts the fixed-point value to an integer value
+    return value;
+}
+
+// void operator<<(const ostream& cout){
+    
+// }
